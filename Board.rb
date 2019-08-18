bd2edcdcf79cac790e27acd39e3710a82b29877a
AGehlot <andrew.gehlot@gmail.com>
2019-08-17 20:38:09 -0400

    Added grid and population methods

--- Board.rb
+++ Board.rb
@@ -3,7 +3,34 @@ require_relative "./Tile.rb"
 class Board
 
     def empty_grid
-        @grid = Array.new(9) { Array.new(9) { Tile.new } }
+        @grid = Array.new(9) { Array.new(9) }
     end
+
+    def populate_board
+        (0...9).each do |row|
+            (0...9).each do |cell|
+                @grid[row][cell] = Tile.new("E")
+            end
+        end
+
+        while num_mines < 10
+            @grid[rand(0...9)][rand(0...9)] = Tile.new("B")
+        end
+
+    end
+
+    def num_mines
+        @grid.flatten.count { |tile| tile.bomb? }
+    end
+
+    def [](arr)
+        @grid[arr[0]][arr[1]]
+    end
+    
+    def []=(position, value)
+        @grid[position[0]][position[1]] = value
+    end
+
+
 
 end
--- Tile.rb
+++ Tile.rb
@@ -1,4 +1,21 @@
+require_relative "./Board.rb"
+
 class Tile
+    attr_reader :value
+    
+    def initialize(value)
+        @value = value
+    end
+
+    def bomb?
+        self.value.include?("B") ? true : false
+    end
+
+    def flag?
+        self.value.include?("F") ? true : false
+    end
+
+
 
 
     
